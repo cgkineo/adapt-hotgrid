02edd0a4de5d6b688a33e2d538dfc6cc4ef28fcf
Guy Willis <guy.willis@kineo.com>
2019-06-20 10:41:59 +0100

    Converted tabs to spaces, indentation amends

--- js/adapt-hotgrid.js
+++ js/adapt-hotgrid.js
@@ -1,11 +1,12 @@
 define([
-    'core/js/adapt',
-    'core/js/models/itemsComponentModel',
-    './hotgridView'
+  'core/js/adapt',
+  'core/js/models/itemsComponentModel',
+  './hotgridView'
 ], function(Adapt, ItemsComponentModel, HotgridView) {
 
-    return Adapt.register('hotgrid', {
-        model: ItemsComponentModel,
-        view: HotgridView
-    });
+  return Adapt.register('hotgrid', {
+    model: ItemsComponentModel,
+    view: HotgridView
+  });
+
 });
