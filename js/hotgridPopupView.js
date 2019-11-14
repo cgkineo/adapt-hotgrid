ade4e10c84e8eeff332164937a06cd4d5dc62bdc
Guy Willis <guywillis@users.noreply.github.com>
2019-11-14 11:28:06 +0000

    Release/v5 (#52)
    * Converted tabs to spaces, indentation amends
    
    * Hotgrid v5 refactor
    
    * AA - fixed list and visited aria
    
    * issue/48 stop responding to ios bad second click event
    
    * Hotgrid AA fixes
    
    * Merged hotgraphic AA amends
    
    * Updated pop up image spacing
    
    * Added new classes field
    
    * issue/48 added comment
    
    * Bower, schema, example, and readme updates
    
    * Moved styling from vanilla to plugin
    
    * Tweaked responsive layout
    
    * Amended item title to use notify title mixin
    
    * Bug fix for visited application responsively
    
    * EM to REM conversion
    
    * Split less files into structural and theme
    
    * Updated license and cross platform coverage
    
    * Removed comments, updated icon background colour

--- js/hotgridPopupView.js
+++ js/hotgridPopupView.js
@@ -1,142 +1,142 @@
 define([
-    'core/js/adapt'
+  'core/js/adapt'
 ], function(Adapt) {
-    'use strict';
+  'use strict';
 
-    var HotgridPopupView = Backbone.View.extend({
+  var HotgridPopupView = Backbone.View.extend({
 
-        className: 'hotgrid-popup',
+    className: 'hotgrid-popup',
 
-        events: {
-            'click .hotgrid-popup-done': 'closePopup',
-            'click .hotgrid-popup-controls': 'onControlClick'
-        },
+    events: {
+      'click .js-hotgrid-popup-close': 'closePopup',
+      'click .js-hotgrid-control-click': 'onControlClick'
+    },
 
-        initialize: function() {
-            // Debounce required as a second (bad) click event is dispatched on iOS causing a jump of two items.
-            this.onControlClick = _.debounce(this.onControlClick.bind(this), 100);
-            this.listenToOnce(Adapt, 'notify:opened', this.onOpened);
-            this.listenTo(this.model.get('_children'), {
-                'change:_isActive': this.onItemsActiveChange,
-                'change:_isVisited': this.onItemsVisitedChange
-            });
-            this.render();
-        },
+    initialize: function() {
+      // Debounce required as a second (bad) click event is dispatched on iOS causing a jump of two items.
+      this.onControlClick = _.debounce(this.onControlClick.bind(this), 100);
+      this.listenToOnce(Adapt, 'notify:opened', this.onOpened);
+      this.listenTo(this.model.get('_children'), {
+        'change:_isActive': this.onItemsActiveChange,
+        'change:_isVisited': this.onItemsVisitedChange
+      });
+      this.render();
+    },
 
-        onOpened: function() {
-            this.applyNavigationClasses(this.model.getActiveItem().get('_index'));
-            this.updatePageCount();
-            this.handleTabs();
-        },
+    onOpened: function() {
+      this.applyNavigationClasses(this.model.getActiveItem().get('_index'));
+      this.updatePageCount();
+      this.handleTabs();
+    },
 
-        applyNavigationClasses: function (index) {
-            var itemCount = this.model.get('_items').length;
-            var canCycleThroughPagination = this.model.get('_canCycleThroughPagination');
+    applyNavigationClasses: function (index) {
+      var itemCount = this.model.get('_items').length;
+      var canCycleThroughPagination = this.model.get('_canCycleThroughPagination');
 
-            var shouldEnableBack = index > 0 || canCycleThroughPagination;
-            var shouldEnableNext = index < itemCount - 1 || canCycleThroughPagination;
-            var $controls = this.$('.hotgrid-popup-controls');
+      var shouldEnableBack = index > 0 || canCycleThroughPagination;
+      var shouldEnableNext = index < itemCount - 1 || canCycleThroughPagination;
+      var $controls = this.$('.hotgrid-popup__controls');
 
-            this.$('hotgrid-popup-nav')
-                .toggleClass('first', !shouldEnableBack)
-                .toggleClass('last', !shouldEnableNext);
+      this.$('hotgrid-popup__nav')
+        .toggleClass('first', !shouldEnableBack)
+        .toggleClass('last', !shouldEnableNext);
 
-            $controls.filter('.back').a11y_cntrl_enabled(shouldEnableBack);
-            $controls.filter('.next').a11y_cntrl_enabled(shouldEnableNext);
-        },
+      $controls.filter('.back').a11y_cntrl_enabled(shouldEnableBack);
+      $controls.filter('.next').a11y_cntrl_enabled(shouldEnableNext);
+    },
 
-        updatePageCount: function() {
-            var template = Adapt.course.get('_globals')._components._hotgrid.popupPagination || '{{itemNumber}} / {{totalItems}}';
-            var labelText = Handlebars.compile(template || '')({
-                itemNumber: this.model.getActiveItem().get('_index') + 1,
-                totalItems: this.model.get('_items').length
-            });
-            this.$('.hotgrid-popup-count').html(labelText);
-        },
+    updatePageCount: function() {
+      var template = Adapt.course.get('_globals')._components._hotgrid.popupPagination || '{{itemNumber}} / {{totalItems}}';
+      var labelText = Handlebars.compile(template || '')({
+          itemNumber: this.model.getActiveItem().get('_index') + 1,
+          totalItems: this.model.get('_items').length
+      });
+      this.$('.hotgrid-popup__count').html(labelText);
+    },
 
-        handleTabs: function() {
-            this.$('.hotgrid-item:not(.active) *').a11y_on(false);
-            this.$('.hotgrid-item.active *').a11y_on(true);
-        },
+    handleTabs: function() {
+      this.$('.hotgrid-popup__item:not(.is-active) *').a11y_on(false);
+      this.$('.hotgrid-popup__item.is-active *').a11y_on(true);
+    },
 
-        onItemsActiveChange: function(item, _isActive) {
-            if (!_isActive) return;
+    onItemsActiveChange: function(item, _isActive) {
+      if (!_isActive) return;
 
-            var index = item.get('_index');
-            this.updatePageCount();
-            this.handleTabs();
-            this.applyItemClasses(index);
-            this.handleFocus(index);
-        },
+      var index = item.get('_index');
+      this.updatePageCount();
+      this.handleTabs();
+      this.applyItemClasses(index);
+      this.handleFocus(index);
+    },
 
-        applyItemClasses: function(index) {
-            this.$('.hotgrid-item[data-index="' + index + '"]').addClass('active').removeAttr('aria-hidden');
-            this.$('.hotgrid-item[data-index="' + index + '"] .hotgrid-content-title').attr("id", "notify-heading");
-            this.$('.hotgrid-item:not([data-index="' + index + '"])').removeClass('active').attr('aria-hidden', 'true');
-            this.$('.hotgrid-item:not([data-index="' + index + '"]) .hotgrid-content-title').removeAttr("id");
-        },
+    applyItemClasses: function(index) {
+      this.$('.hotgrid-popup__item[data-index="' + index + '"]').addClass('is-active').removeAttr('aria-hidden');
+      this.$('.hotgrid-popup__item[data-index="' + index + '"] .hotgrid-popup__item-title').attr("id", "notify-heading");
+      this.$('.hotgrid-popup__item:not([data-index="' + index + '"])').removeClass('is-active').attr('aria-hidden', 'true');
+      this.$('.hotgrid-popup__item:not([data-index="' + index + '"]) .hotgrid-popup__item-title').removeAttr("id");
+    },
 
-        handleFocus: function(index) {
-            this.$('.hotgrid-popup-inner .active').a11y_focus();
-            this.applyNavigationClasses(index);
-        },
+    handleFocus: function(index) {
+      this.$('.hotgrid-popup__inner .is-active').a11y_focus();
+      this.applyNavigationClasses(index);
+    },
 
-        onItemsVisitedChange: function(item, _isVisited) {
-            if (!_isVisited) return;
+    onItemsVisitedChange: function(item, _isVisited) {
+      if (!_isVisited) return;
 
-            this.$('.hotgrid-item')
-                .filter('[data-index="' + item.get('_index') + '"]')
-                .addClass('visited');
-        },
+      this.$('.hotgrid-popup__item')
+        .filter('[data-index="' + item.get('_index') + '"]')
+        .addClass('is-visited');
+    },
 
-        render: function() {
-            var data = this.model.toJSON();
-            data.view = this;
-            var template = Handlebars.templates['hotgridPopup'];
-            this.$el.html(template(data));
-        },
+    render: function() {
+      var data = this.model.toJSON();
+      data.view = this;
+      var template = Handlebars.templates['hotgridPopup'];
+      this.$el.html(template(data));
+    },
 
-        closePopup: function(event) {
-            Adapt.trigger('notify:close');
-        },
+    closePopup: function(event) {
+      Adapt.trigger('notify:close');
+    },
 
-        onControlClick: function(event) {
-            event.preventDefault();
+    onControlClick: function(event) {
+      event.preventDefault();
 
-            var direction = $(event.currentTarget).hasClass('back') ? 'back' : 'next';
-            var index = this.getNextIndex(direction);
+      var direction = $(event.currentTarget).hasClass('back') ? 'back' : 'next';
+      var index = this.getNextIndex(direction);
 
-            if (index !== -1) {
-                this.setItemState(index);
-            }
-        },
+      if (index !== -1) {
+        this.setItemState(index);
+      }
+    },
 
-        getNextIndex: function(direction) {
-            var index = this.model.getActiveItem().get('_index');
-            var lastIndex = this.model.get('_items').length - 1;
+    getNextIndex: function(direction) {
+      var index = this.model.getActiveItem().get('_index');
+      var lastIndex = this.model.get('_items').length - 1;
 
-            switch (direction) {
-                case 'back':
-                    if (index > 0) return --index;
-                    if (this.model.get('_canCycleThroughPagination')) return lastIndex;
-                    break;
-                case 'next':
-                    if (index < lastIndex) return ++index;
-                    if (this.model.get('_canCycleThroughPagination')) return 0;
-            }
-            return -1;
-        },
+      switch (direction) {
+        case 'back':
+          if (index > 0) return --index;
+          if (this.model.get('_canCycleThroughPagination')) return lastIndex;
+          break;
+        case 'next':
+          if (index < lastIndex) return ++index;
+          if (this.model.get('_canCycleThroughPagination')) return 0;
+      }
+      return -1;
+    },
 
-        setItemState: function(index) {
-            this.model.getActiveItem().toggleActive();
+    setItemState: function(index) {
+      this.model.getActiveItem().toggleActive();
 
-            var nextItem = this.model.getItem(index);
-            nextItem.toggleActive();
-            nextItem.toggleVisited(true);
-        }
+      var nextItem = this.model.getItem(index);
+      nextItem.toggleActive();
+      nextItem.toggleVisited(true);
+    }
 
-    });
+  });
 
-    return HotgridPopupView;
+  return HotgridPopupView;
 
 });
