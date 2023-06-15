af57a35fc1f8a28d1a22446395a30ea0764ec301
Brad Simpson <brad.simpson@kineo.com>
2023-06-15 10:17:45 -0600

    Only show plus icon if _showPlusIcon is enabled

--- templates/hotgrid.jsx
+++ templates/hotgrid.jsx
@@ -75,7 +75,7 @@ export default function Hotgrid(props) {
                       <img className="hotgrid__item-image is-visited" src={_graphic.srcVisited} />
                     }
 
-                    {(_showPlusIcon || !_graphic.title || (_graphic.title && _isVisited)) &&
+                    {(_showPlusIcon || _isVisited) &&
                     <span className="btn-icon hotgrid__item-icon" aria-hidden="true">
                       <span className="icon" />
                     </span>
